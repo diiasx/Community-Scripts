4436e7fdd5c86f84bda7fa012ccf711ee472e9a4
diiasx <yagotdias2702@gmail.com>
2026-01-30 01:19:38 -0300

    Update script_list.lua

--- script_list.lua
+++ script_list.lua
@@ -3,12 +3,15 @@ script_manager = {
     actualVersion = 0.4,
     _cache = {
 
+        Dbo = {
+
     ['Single + AOE Safe'] = {
       url = 'https://raw.githubusercontent.com/diiasx/Community-Scripts/main/Scripts/Dbo/single_aoe_safe.lua',
       author = 'Yago',
       description = 'Usa AOE ou single baseado em players e quantidade de mobs',
       enabled = false
     },
+             },
 
     ['Smart Lure Control'] = {
       url = 'https://raw.githubusercontent.com/diiasx/Community-Scripts/main/Scripts/smart_lure.lua',
